749b04061f5c27c3a95b49377d2d61c8528906d2
cipher <120999508+Primi373-creator@users.noreply.github.com>
2024-03-28 03:38:08 +0000

    hey

--- config.js
+++ config.js
@@ -1,34 +1,25 @@
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
-
 if (fs.existsSync("config.env")) {
   require("dotenv").config({ path: "./config.env" });
 }
-
 const toBool = (x) => x === "true";
 
 const DATABASE_URL = process.env.DATABASE_URL || "./assets/database.db";
 
 module.exports = {
-  ANTILINK: toBool(process.env.ANTI_LINK) || false,
+  BASE_URL: "https://api.alpha-md.rf.gd/",
+  ALPHA_KEY: process.env.ALPHA_KEY || "alpha-free",
   LOGS: toBool(process.env.LOGS) || true,
-  ANTILINK_ACTION: process.env.ANTI_LINK || "kick",
   SESSION_ID: process.env.SESSION_ID || "idvu3oj6_XASENA_w46_XASENA_nb8c",
   LANG: process.env.LANG || "EN",
   HANDLERS:
     process.env.HANDLER === "false" || process.env.HANDLER === "null"
       ? "^"
       : "^[#]",
-  RMBG_KEY: process.env.RMBG_KEY || false,
   BRANCH: "master",
   WARN_COUNT: 3,
-  PACKNAME: process.env.PACKNAME || "X-asena",
-  WELCOME_MSG: process.env.WELCOME_MSG || "Hi @user Welcome to @gname",
-  GOODBYE_MSG: process.env.GOODBYE_MSG || "Hi @user It was Nice Seeing you",
-  AUTHOR: process.env.AUTHOR || "X-electra",
   SUDO: process.env.SUDO || "918765432109",
-  HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || "",
-  HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
   OWNER_NAME: process.env.OWNER_NAME || "Neeraj-X0",
   BOT_NAME: process.env.BOT_NAME || "X-asena",
   WORK_TYPE: process.env.WORK_TYPE || "public",
